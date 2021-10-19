650dbef2dcb24b545bd6631abf72d4e9a1eb0ebc
VadDoc <vadprudnikov@gmail.com>
2021-10-19 19:27:18 +0300

    Add Task 9

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import HW7 from "../../h7/HW7";
 import HW8 from "../../h8/HW8";
+import HW9 from "../../h9/HW9";
 
 type PropsType = {
   title: string
@@ -12,7 +13,7 @@ function Junior(props: PropsType) {
       <h1>{props.title}</h1>
       <HW7/>
       <HW8/>
-      {/*<HW9/>*/}
+      <HW9/>
       {/*<HW10/>*/}
     </div>
   )
